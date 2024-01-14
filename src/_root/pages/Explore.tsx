0f1fbba19182287f332f72c26936fa6b6bada5e5
07Abhinav <108852841+07Abhinav@users.noreply.github.com>
2024-01-14 23:23:17 +0530

    Update Explore.tsx

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -47,7 +47,7 @@ const Explore = () => {
     );
 
   const shouldShowSearchResults = searchValue !== "";
-  const shouldShowPosts = !shouldShowSearchResults && posts.pages.every((item) => item.documents.length === 0);
+  const shouldShowPosts = !shouldShowSearchResults && posts.pages.every((item) => item && item.documents.length === 0);
 
   return (
     <div className="explore-container">
@@ -97,7 +97,10 @@ const Explore = () => {
           <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
         ) : (
           posts.pages.map((item, index) => (
-            <GridPostList key={`page-${index}`} posts={item.documents} />
+            // Add a type check to ensure 'item' is defined
+            item && (
+              <GridPostList key={`page-${index}`} posts={item.documents} />
+            )
           ))
         )}
       </div>
